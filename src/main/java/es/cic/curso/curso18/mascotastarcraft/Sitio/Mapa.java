e110352428abc123bb7a96515f13aa219bd81af8
Christian Muñoz Asón <maquina1995@gmail.com>
2017-02-05 16:48:40 +0100

    Agregado atributo boolean a mapa

--- src/main/java/es/cic/curso/curso18/mascotastarcraft/Sitio/Mapa.java
+++ src/main/java/es/cic/curso/curso18/mascotastarcraft/Sitio/Mapa.java
@@ -2,6 +2,7 @@ package es.cic.curso.curso18.mascotastarcraft.Sitio;
 
 import es.cic.curso.curso18.mascotastarcraft.dominio.Identificable;
 import es.cic.curso.curso18.mascotastarcraft.fortaleza.Base;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -31,6 +32,9 @@ public class Mapa implements Identificable<Long> {
     
     @Column(name = "jugadoresMaximo")
     private int jugadoresMaximo;
+    
+    @Column(name = "jugado")
+    private boolean jugado;
 
     @JoinColumn(name = "idBase")
     @ManyToOne(fetch = FetchType.LAZY)
@@ -39,11 +43,12 @@ public class Mapa implements Identificable<Long> {
     public Mapa() {
     }
 
-    public Mapa(String nombreMapa, int basesMaximo, int jugadoresMaximo, Base base) {
+    public Mapa(String nombreMapa, int basesMaximo, int jugadoresMaximo,boolean jugado, Base base) {
         this.nombreMapa = nombreMapa;
         this.basesMaximo = basesMaximo;
         this.jugadoresMaximo = jugadoresMaximo;
         this.base = base;
+        this.jugado=jugado;
     }
 
     @Override
@@ -88,5 +93,57 @@ public class Mapa implements Identificable<Long> {
         this.base = base;
     }
 
+    public boolean isJugado() {
+        return jugado;
+    }
+
+    public void setJugado(boolean jugado) {
+        this.jugado = jugado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.idMapa);
+        hash = 41 * hash + Objects.hashCode(this.nombreMapa);
+        hash = 41 * hash + this.basesMaximo;
+        hash = 41 * hash + this.jugadoresMaximo;
+        hash = 41 * hash + (this.jugado ? 1 : 0);
+        hash = 41 * hash + Objects.hashCode(this.base);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Mapa other = (Mapa) obj;
+        if (this.basesMaximo != other.basesMaximo) {
+            return false;
+        }
+        if (this.jugadoresMaximo != other.jugadoresMaximo) {
+            return false;
+        }
+        if (this.jugado != other.jugado) {
+            return false;
+        }
+        if (!Objects.equals(this.nombreMapa, other.nombreMapa)) {
+            return false;
+        }
+        if (!Objects.equals(this.idMapa, other.idMapa)) {
+            return false;
+        }
+        if (!Objects.equals(this.base, other.base)) {
+            return false;
+        }
+        return true;
+    }
     
 }
