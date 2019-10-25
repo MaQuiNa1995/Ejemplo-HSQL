2b724bc7ebb3e6dd7805b4c684d2c2accec78d34
MaQuiNa1995 <maquina1995@gmail.com>
2019-10-26 00:42:34 +0200

    javadoc de un error de Junit y spring  + transacciones

--- src/test/java/es/maquina1995/hsqldb/repository/CrudRepositoryImplTest.java
+++ src/test/java/es/maquina1995/hsqldb/repository/CrudRepositoryImplTest.java
@@ -15,9 +15,32 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Clase de test genérica para hacer test al vuelo de método tipicos de un CRUD
+ * <p>
+ * Lecciones Aprendidas:
+ * <p>
+ * si no pones el {@link TransactionalTestExecutionListener} spring no puede
+ * crear el transactionManager en test con este error me tiré 1 tarde entera
+ * <p>
+ * Me daba un error de:
+ * <p>
+ * javax.persistence.TransactionRequiredException: No EntityManager with actual
+ * transaction available for current thread - cannot reliably process 'persist'
+ * call
+ * <p>
+ * <a href=
+ * "https://github.com/MaQuiNa1995/Ejemplo-HSQL/commit/2cb42961e4834f20a3b772cd64a3cee5547f6326">Este
+ * es el commit</a> donde arreglé el error
+ * 
+ * @author MaQuiNa1995
+ *
+ * @param <K> Genérico que representa un objeto que sea hijo de {@link Number}
+ * @param <T> Genérico que representa un objeto que esté anotado con
+ *        {@link Entity}
+ */
 @ExtendWith(SpringExtension.class)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class })
-
 public abstract class CrudRepositoryImplTest<K extends Number, T extends Identificable<K>> {
 
     @PersistenceContext
