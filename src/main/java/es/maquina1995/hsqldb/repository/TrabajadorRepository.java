69ccf9d8b2f0dca1465d826d2d1b99e43b082a64
MaQuiNa1995 <maquina1995@gmail.com>
2019-11-02 21:52:44 +0100

    mas eliminaciones de archivos para empezar de cero

--- src/main/java/es/maquina1995/hsqldb/repository/TrabajadorRepository.java
+++
@@ -1,7 +0,0 @@
-package es.maquina1995.hsqldb.repository;
-
-import es.maquina1995.hsqldb.dominio.Trabajador;
-
-public interface TrabajadorRepository extends CrudRepository<Long, Trabajador> {
-
-}
