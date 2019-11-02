9036326f48f8ce9f364a1deb8d7a0419c50c81e6
MaQuiNa1995 <maquina1995@gmail.com>
2019-11-02 22:27:16 +0100

    Añadidos 2 nuevas entidades al dominio para ejemplo 1:1

---
+++ src/main/java/es/maquina1995/hsqldb/dominio/Rango.java
@@ -0,0 +1,46 @@
+package es.maquina1995.hsqldb.dominio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import es.maquina1995.hsqldb.repository.Persistible;
+
+@Entity
+@Table(name = "RANGO")
+public class Rango implements Persistible<Long> {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @Override
+    public Long getId() {
+	return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public String getNombre() {
+	return nombre;
+    }
+
+    public void setNombre(String nombre) {
+	this.nombre = nombre;
+    }
+
+}
