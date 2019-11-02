863d568ac0b944c98e25d0af9de1f2f0b8d2f95d
MaQuiNa1995 <maquina1995@gmail.com>
2019-11-02 21:45:09 +0100

    Eliminación masiva para empezar con 1 nuevo enfoque el proyecto

--- src/main/java/es/maquina1995/hsqldb/repository/BaseRepository.java
+++
@@ -1,7 +0,0 @@
-package es.maquina1995.hsqldb.repository;
-
-import es.maquina1995.hsqldb.dominio.Base;
-
-public interface BaseRepository extends CrudRepository<Long, Base> {
-
-}
--- src/main/java/es/maquina1995/hsqldb/repository/MapaRepository.java
+++
@@ -1,7 +0,0 @@
-package es.maquina1995.hsqldb.repository;
-
-import es.maquina1995.hsqldb.dominio.Mapa;
-
-public interface MapaRepository extends CrudRepository<Long, Mapa> {
-
-}
